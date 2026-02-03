0280f7484f7287874c2962a361ca16dcf393b39b
yash-dynamo <yash@syndr.com>
2026-02-03 14:30:05 +0530

    added contract addresses

---
+++ constants/contracts.go
@@ -0,0 +1,11 @@
+package constants
+
+// Deployed contract addresses on chain ID 11155111 (Sepolia)
+const (
+	MockUSDCAddress         = "0x46696d621F40904ae75BCabaBf5E7Da6768Fe5a8"
+	PredictionMarketAddress = "0x52d6ae3F85cF0DaE4800C240FB0D80fE0E5b21C2"
+
+	OutcomeTokenAAddress = "0xC695C89771786864a368714cCE9BfA1Ac5A3515D" 
+	OutcomeTokenBAddress = "0x888A55C56eDc4Eac9cE1E9F2b18c7EAbc3F95db7" 
+	OutcomeTokenCAddress = "0xC3e774bD95FD3864007dA15CB29b1263057D3A09" 
+)
